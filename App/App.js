31754d888c76ec4a5909187929d02a5476bc4c9d
Al-Shaikh-Ul-Habibi <56868780+jaffery0605@users.noreply.github.com>
2020-06-15 15:50:46 +0530

    updated with dark theme

--- App/App.js
+++ App/App.js
@@ -1,9 +1,45 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer,DarkTheme,DefaultTheme } from '@react-navigation/native';
 import RootStackScreen from './src/Screens/RootStackScreen';
+import {Provider as PaperProvider,DarkTheme as PaperDarkTheme,DefaultTheme as PaperDefaultTheme} from 'react-native-paper';
+import {AuthContext} from './src/Screens/Context';
+
+const CustomDefaultTheme={
+  ...DefaultTheme,
+  ...PaperDefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    ...PaperDefaultTheme.colors,
+    background: '#fff',
+    text: '#111'
+  }
+}
+const CustomDarkTheme={
+  ...DarkTheme,
+  ...PaperDarkTheme,
+  colors:{
+    ...DarkTheme.colors,
+    ...PaperDarkTheme.colors,
+    background: '#111',
+    text: '#fff'
+  }
+}
+
 
 
 export default function App() {
+  
+  const [isDarkTheme,setIsDarkTheme] = React.useState(false);
+  const authContext = React.useMemo(()=>({
+    toggleTheme: () =>{
+      setIsDarkTheme(isDarkTheme=>!isDarkTheme);
+    }
+  }),[])
+
+  
+const theme = isDarkTheme ? CustomDarkTheme : CustomDefaultTheme
+
   // const [isLoading, setLoading] = React.useState(true);
 
   // if(isLoading){
@@ -15,8 +51,13 @@ export default function App() {
   // }
   
   return (
-    <NavigationContainer>
+    <PaperProvider theme={theme}>
+    <AuthContext.Provider value={authContext}>
+    <NavigationContainer theme={theme}>
     <RootStackScreen/>
     </NavigationContainer>
+    </AuthContext.Provider>
+    </PaperProvider>
+    
   );
 }
